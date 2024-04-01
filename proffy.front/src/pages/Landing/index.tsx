63b6f87ce9f9402544dfbb984155a59d94e7c562
Alerrandro Tomé <53446642+AlerrandroTome@users.noreply.github.com>
2024-03-31 23:45:40 -0300

    Start front project and add landing page

---
+++ proffy.front/src/pages/Landing/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+
+function Landing() {
+    return (
+    <div id="page-landing">
+        <div id="page-landing-content" className="container">
+            <div className="logo-container">
+                <img src={logoImg} alt="Proffy" />
+                <h2>Your online study platform.</h2>
+            </div>
+
+            <img src={landingImg} alt="Study Patform" className="hero-image" />
+            <div className="buttons-container">
+                <a href="" className="study">
+                    <img src={studyIcon} alt="Study" />
+                    Study
+                </a>
+                <a href="" className="give-classes">
+                    <img src={giveClassesIcon} alt="" />
+                    Give Classes
+                </a>
+            </div>
+
+            <span className="total-connections">
+                Total of 200 connections already made
+                <img src={purpleHeartIcon} alt="Purple Heart" />
+            </span>
+        </div>
+    </div>    
+    );
+}
+
+export default Landing;
